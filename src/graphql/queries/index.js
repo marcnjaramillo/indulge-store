0ddf3dd16dd7c5deb29f8663194405f1411c063b
marcnjaramillo <mnj.webdeveloper@gmail.com>
2021-10-08 03:11:35 -0700

    Add new query for cart

--- src/graphql/queries/index.js
+++ src/graphql/queries/index.js
@@ -1,2 +1,3 @@
 export { default as GET_ALL_PRODUCTS_QUERY } from './getAllProducts'
 export { default as GET_PRODUCT_QUERY } from './getProduct'
+export { default as GET_CART_QUERY } from './getCart'
