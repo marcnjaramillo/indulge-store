36aed2afd2c41c845382c814c7016740dbf0e31e
marcnjaramillo <mnj.webdeveloper@gmail.com>
2021-10-08 03:11:17 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { ApolloProvider } from '@apollo/client';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { ApolloProvider } from '@apollo/client'
 
 import { client } from './apollo/client'
-import App from './App';
-import './index.css';
+import App from './App'
+import './index.css'
 
 ReactDOM.render(
   <ApolloProvider client={client}>
     <App />
   </ApolloProvider>,
   document.getElementById('root')
-);
+)
 
