bec0a38de6929b9a505d7f637f95db826c6f0e4c
marcnjaramillo <mnj.webdeveloper@gmail.com>
2021-10-08 03:12:13 -0700

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,12 @@
-// import { render, screen } from '@testing-library/react';
-// import App from './App';
+import { render, screen } from '@testing-library/react'
+import App from './App'
+import { client } from './apollo/client'
+import { ApolloProvider } from '@apollo/client'
 
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
+test('renders loading message', () => {
+  render(<ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>)
+  const loadingMessage = screen.getByText(/loading/i)
+  expect(loadingMessage).toBeInTheDocument()
+})
