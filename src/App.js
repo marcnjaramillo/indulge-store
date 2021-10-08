5f49004d1567ad7285d0f4d726aedcc5f4eb4af6
marcnjaramillo <mnj.webdeveloper@gmail.com>
2021-10-08 03:06:17 -0700

    Add cart functionality

--- src/App.js
+++ src/App.js
@@ -1,14 +1,72 @@
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { useQuery, useMutation } from '@apollo/client';
+import { useState, useEffect } from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { useQuery, useMutation } from '@apollo/client'
 
 import { GET_ALL_PRODUCTS_QUERY } from './graphql/queries'
 import { normalizeProduct } from './graphql/shopify/utils'
-import { Homepage, Navigation, Products } from './components'
-import './App.scss';
+import {
+  cartLinesAdd,
+  cartLinesRemove,
+  cartLinesUpdate,
+  createCart,
+  useCartEffect
+} from './graphql/mutations';
+import { Homepage, Navigation, Products, Cart } from './components'
+import './App.scss'
 
 const App = () => {
+  const [cart, setCart] = useState({ lines: { edges: [] } })
 
-  const { loading: shopLoading, error: shopError, data: shopData } = useQuery(GET_ALL_PRODUCTS_QUERY)
+  const [createCartMutation,
+    { data: createCartData }] = useMutation(createCart);
+
+  const [cartLinesAddMutation,
+    { data: cartLinesAddData }] = useMutation(cartLinesAdd);
+
+  const [cartLinesUpdateMutation,
+    { data: cartLinesUpdateData }] = useMutation(cartLinesUpdate)
+
+  const [cartLinesRemoveMutation,
+    { data: cartLinesRemoveData }] = useMutation(cartLinesRemove)
+
+  const {
+    loading: shopLoading, error: shopError, data: shopData
+  } = useQuery(GET_ALL_PRODUCTS_QUERY)
+
+  useEffect(() => {
+    const variables = { input: {} }
+    createCartMutation({ variables }).then(
+      res => {
+        console.log(res)
+      },
+      err => {
+        console.log('create cart error', err)
+      }
+    )
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  useCartEffect(createCartData, 'cartCreate', setCart)
+  useCartEffect(cartLinesAddData, 'cartLinesAdd', setCart)
+  useCartEffect(cartLinesUpdateData, 'cartLinesUpdate', setCart)
+  useCartEffect(cartLinesRemoveData, 'cartLinesRemove', setCart)
+
+  const addToCart = (merchandiseId, quantity) => {
+    const variables = { cartId: cart.id, lines: [{ merchandiseId, quantity: parseInt(quantity, 10) }] }
+
+    cartLinesAddMutation({ variables })
+  }
+
+  const updateCartLines = (lineId, merchandiseId, quantity) => {
+    const variables = { cartId: cart.id, lines: [{ id: lineId, merchandiseId, quantity: parseInt(quantity, 10) }] }
+    cartLinesUpdateMutation({ variables })
+  };
+
+  const removeCartLines = (lineId) => {
+    const variables = { cartId: cart.id, lineIds: [lineId] }
+    cartLinesRemoveMutation({ variables })
+  }
 
   if (shopLoading) {
     return <p>Loading...</p>
@@ -32,11 +90,26 @@ const App = () => {
           <Route exact path='/'>
             <Homepage products={products} />
           </Route>
-          <Route path='/:category' children={<Products products={products} />} />
+          <Route exact path='/cart'>
+            <Cart
+              cart={cart}
+              updateCartLines={updateCartLines}
+              removeCartLines={removeCartLines}
+            />
+          </Route>
+          <Route path='/:category'
+            children={
+              <Products
+                products={products}
+                cart={cart}
+                addToCart={addToCart}
+              />
+            }
+          />
         </Switch>
       </Router>
     </div>
-  );
+  )
 }
 
 export default App
