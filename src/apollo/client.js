69e07fc94cbec9cb4ac5eb9edebd015110177400
marcnjaramillo <mnj.webdeveloper@gmail.com>
2021-10-08 03:04:56 -0700

    Update api version

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client'
 
 export const client = new ApolloClient({
-  uri: 'https://mnj-webdevelopment.myshopify.com/api/2021-07/graphql.json',
+  uri: 'https://mnj-webdevelopment.myshopify.com/api/2021-10/graphql.json',
   headers: {
     'Content-Type': 'application/json',
     'X-Shopify-Storefront-Access-Token': '91b47ffc3e0c6130bc24e7df78c26761'
