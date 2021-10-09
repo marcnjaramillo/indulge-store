394efec820f7b256759894c97dfaac6d1e92de7d
marcnjaramillo <mnj.webdeveloper@gmail.com>
2021-10-08 20:45:56 -0700

    Edit line item and add styles

--- src/components/line-item/LineItem.js
+++ src/components/line-item/LineItem.js
@@ -1,22 +1,27 @@
-import React from 'react'
+import {useState} from 'react'
+import styles from './LineItem.module.scss'
 
 function LineItem({ line_item, removeCartLines, updateCartLines }) {
+
+  const [lineQuantity, setLineQuantity] = useState(line_item.quantity)
 
   const decrementQuantity = (lineId, merchandiseId) => {
     const updatedQuantity = line_item.quantity - 1
     updateCartLines(lineId, merchandiseId, updatedQuantity)
+    setLineQuantity(updatedQuantity)
   }
 
   const incrementQuantity = (lineId, merchandiseId) => {
     const updatedQuantity = line_item.quantity + 1
     updateCartLines(lineId, merchandiseId, updatedQuantity)
+    setLineQuantity(updatedQuantity)
   }
 
   return (
-    <li className="Line-item">
-      <div className="Line-item__img">
+    <li className={styles.cartLine}>
+      <div className={styles.imageContainer}>
         <img
-          // className={s.productImage}
+          className={styles.productImage}
           alt={`${line_item.title}`}
           src={line_item.merchandise.image.originalSrc}
           height={220}
@@ -24,17 +29,19 @@ function LineItem({ line_item, removeCartLines, updateCartLines }) {
           quality='85'
         />
       </div>
-      <div className="Line-item__content">
-        <div className="Line-item__content-row">
+      <div className={styles.lineDetails}>
+        <div className={styles.lineTitle}>
           <span className="Line-item__title">
             {line_item.merchandise.title}
           </span>
         </div>
-        <div className="Line-item__content-row">
+        <div className={styles.lineRow}>
           <div className="Line-item__quantity-container">
-            <button className="Line-item__quantity-update" onClick={() => decrementQuantity(line_item.id, line_item.merchandise.id)}>-</button>
-            <span className="Line-item__quantity">{line_item.quantity}</span>
-            <button className="Line-item__quantity-update" onClick={() => incrementQuantity(line_item.id, line_item.merchandise.id)}>+</button>
+          <div className="input-group">
+            <button className="btn btn-outline-secondary" type="button" onClick={() => decrementQuantity(line_item.id, line_item.merchandise.id)}>-</button>
+            <span className={styles.quantity}>{lineQuantity}</span>
+            <button className="btn btn-outline-secondary" type="button" onClick={() => incrementQuantity(line_item.id, line_item.merchandise.id)}>+</button>
+          </div>
           </div>
           <span className="Line-item__price">
             ${(line_item.quantity * line_item.merchandise.price.amount).toFixed(2)}
