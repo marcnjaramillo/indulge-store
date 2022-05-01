5b9dadf688feaefcf68d609db6109401cc3aa312
Marc Jaramillo <mnj.webdeveloper@gmail.com>
2022-05-01 09:42:54 -0700

    Update styles for cart and checkout modal

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -1,28 +1,35 @@
-import { useState } from 'react'
-import { useQuery } from '@apollo/client'
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import {
+  QuestionMarkCircleIcon
+} from '@heroicons/react/solid';
 
-import { GET_CART_QUERY } from '../../graphql/queries'
+import { useQuery } from '@apollo/client';
 
-import { LineItem, CheckoutModal } from '../'
-import { CardIcon } from '../icons'
+import { GET_CART_QUERY } from '../../graphql/queries';
 
-import styles from './Cart.module.scss'
+import { LineItem, CheckoutModal } from '../';
+
+import styles from './Cart.module.scss';
 
 const Cart = ({ cart, removeCartLines, updateCartLines }) => {
-
   const [modalShow, setModalShow] = useState(false);
 
-  const { loading: cartLoading, error: cartError, data: cartData } = useQuery(GET_CART_QUERY, { variables: { cartId: cart.id } })
+  const {
+    loading: cartLoading,
+    error: cartError,
+    data: cartData,
+  } = useQuery(GET_CART_QUERY, { variables: { cartId: cart.id } });
 
   if (cartLoading) {
-    return <p>Loading...</p>
+    return <p>Loading...</p>;
   }
 
   if (cartError) {
-    return <p>{cartError.message}</p>
+    return <p>{cartError.message}</p>;
   }
 
-  console.log(cartData)
+  console.log(cartData);
 
   const line_items = cartData.cart.lines.edges.map((line_item) => {
     return (
@@ -32,53 +39,159 @@ const Cart = ({ cart, removeCartLines, updateCartLines }) => {
         key={line_item.node.id}
         line_item={line_item.node}
       />
-    )
-  })
+    );
+  });
 
   const onShow = () => {
-    setModalShow(true)
-  }
+    setModalShow(true);
+  };
 
   const onHide = () => {
-    setModalShow(false)
-    window.location.replace('/')
-  }
+    setModalShow(false);
+    window.location.replace('/');
+  };
 
   return (
     <div>
-      <header className={styles.cartHeader}>
-        <h2>Your Cart</h2>
-      </header>
       <CheckoutModal show={modalShow} onHide={onHide} />
-      {line_items.length === 0 ? (
-        <p className={styles.emptyCart}>Your cart is empty.</p>
-      ) : (
-        <>
-          <ul className={styles.linesContainer}>
-            {line_items}
-          </ul>
-          <section className={styles.cartDetails}>
-            <div className={styles.cartInfo}>
-              <span className={styles.subtotal}>Subtotal</span>
-              <span className={styles.price}>${parseFloat(cart.estimatedCost.subtotalAmount?.amount).toFixed(2)}</span>
-            </div>
-            <div className={styles.cartInfo}>
-              <span className={styles.taxes}>Taxes</span>
-              <span className={styles.price}>${parseFloat(cart.estimatedCost.totalTaxAmount?.amount).toFixed(2)}</span>
-            </div>
-            <div className={styles.cartInfo}>
-              <span className={styles.total}><strong>Total</strong></span>
-              <span className={styles.price}><strong>${parseFloat(cart.estimatedCost.totalAmount?.amount).toFixed(2)}</strong></span>
+      <main className='max-w-2xl mx-auto pt-16 pb-24 px-4 sm:px-6 lg:max-w-7xl lg:px-8'>
+        <h1 className='text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl'>
+          Shopping Cart
+        </h1>
+
+        <div className='mt-12 lg:grid lg:grid-cols-12 lg:gap-x-12 lg:items-start xl:gap-x-16'>
+          <section aria-labelledby='cart-heading' className='lg:col-span-7'>
+            <h2 id='cart-heading' className='sr-only'>
+              Items in your shopping cart
+            </h2>
+
+            {line_items.length === 0 ? (
+              <p className={styles.emptyCart}>Your cart is empty.</p>
+            ) : (
+              <ul className='border-t border-b border-gray-200 divide-y divide-gray-200'>
+                {line_items}
+              </ul>
+            )}
+          </section>
+
+          {/* Order summary */}
+          <section
+            aria-labelledby='summary-heading'
+            className='mt-16 bg-gray-50 rounded-lg px-4 py-6 sm:p-6 lg:p-8 lg:mt-0 lg:col-span-5'>
+            <h2
+              id='summary-heading'
+              className='text-lg font-medium text-gray-900'>
+              Order summary
+            </h2>
+
+            <dl className='mt-6 space-y-4'>
+              <div className='flex items-center justify-between'>
+                <dt className='text-sm text-gray-600'>Subtotal</dt>
+                <dd className='text-sm font-medium text-gray-900'>
+                  $
+                  {parseFloat(
+                    cart.estimatedCost.subtotalAmount?.amount
+                  ).toFixed(2)}
+                </dd>
+              </div>
+              <div className='border-t border-gray-200 pt-4 flex items-center justify-between'>
+                <dt className='flex items-center text-sm text-gray-600'>
+                  <span>Shipping estimate</span>
+                  <Link
+                    to='#'
+                    className='ml-2 flex-shrink-0 text-gray-400 hover:text-gray-500'>
+                    <span className='sr-only'>
+                      Learn more about how shipping is calculated
+                    </span>
+                    <QuestionMarkCircleIcon
+                      className='h-5 w-5'
+                      aria-hidden='true'
+                    />
+                  </Link>
+                </dt>
+                <dd className='text-sm font-medium text-gray-900'>$5.00</dd>
+              </div>
+              <div className='border-t border-gray-200 pt-4 flex items-center justify-between'>
+                <dt className='flex text-sm text-gray-600'>
+                  <span>Tax estimate</span>
+                  <Link
+                    to='#'
+                    className='ml-2 flex-shrink-0 text-gray-400 hover:text-gray-500'>
+                    <span className='sr-only'>
+                      Learn more about how tax is calculated
+                    </span>
+                    <QuestionMarkCircleIcon
+                      className='h-5 w-5'
+                      aria-hidden='true'
+                    />
+                  </Link>
+                </dt>
+                <dd className='text-sm font-medium text-gray-900'>
+                  $
+                  {parseFloat(
+                    cart.estimatedCost.totalTaxAmount?.amount
+                  ).toFixed(2)}
+                </dd>
+              </div>
+              <div className='border-t border-gray-200 pt-4 flex items-center justify-between'>
+                <dt className='text-base font-medium text-gray-900'>
+                  Order total
+                </dt>
+                <dd className='text-base font-medium text-gray-900'>
+                  $
+                  {parseFloat(cart.estimatedCost.totalAmount?.amount).toFixed(
+                    2
+                  )}
+                </dd>
+              </div>
+            </dl>
+
+            <div className='mt-6'>
+              <button
+                type='submit'
+                className='w-full border border-transparent rounded-md shadow-sm py-3 px-4 text-base font-medium text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-50 focus:ring-indigo-500 bg-gradient-to-b from-indigo-500 to-purple-500 hover:from-sky-600 hover:to-cyan-400'
+                onClick={onShow}>
+                Checkout
+              </button>
             </div>
           </section>
-          <div className={styles.checkoutContainer}>            
-            <button bsPrefix='checkoutButton' className={styles.checkoutButton} onClick={onShow}><CardIcon />Checkout</button>
-          </div>
-        </>
-      )
-      }
-    </div>
-  )
-}
+        </div>
 
-export default Cart
+        {/* Related products */}
+        {/* <section aria-labelledby="related-heading" className="mt-24">
+          <h2 id="related-heading" className="text-lg font-medium text-gray-900">
+            You may also like&hellip;
+          </h2>
+
+          <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+            {relatedProducts.map((relatedProduct) => (
+              <div key={relatedProduct.id} className="group relative">
+                <div className="w-full min-h-80 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
+                  <img
+                    src={relatedProduct.imageSrc}
+                    alt={relatedProduct.imageAlt}
+                    className="w-full h-full object-center object-cover lg:w-full lg:h-full"
+                  />
+                </div>
+                <div className="mt-4 flex justify-between">
+                  <div>
+                    <h3 className="text-sm text-gray-700">
+                      <a href={relatedProduct.href}>
+                        <span aria-hidden="true" className="absolute inset-0" />
+                        {relatedProduct.name}
+                      </a>
+                    </h3>
+                    <p className="mt-1 text-sm text-gray-500">{relatedProduct.color}</p>
+                  </div>
+                  <p className="text-sm font-medium text-gray-900">{relatedProduct.price}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </section> */}
+      </main>
+    </div>
+  );
+};
+
+export default Cart;
--- src/components/line-item/LineItem.js
+++ src/components/line-item/LineItem.js
@@ -1,60 +1,123 @@
-import {useState} from 'react'
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import {
+  MinusIcon,
+  PlusIcon,
+  TrashIcon,
+} from '@heroicons/react/outline';
 
-import { TrashIcon } from '../icons'
-
-import styles from './LineItem.module.scss'
-
-function LineItem({ line_item, removeCartLines, updateCartLines }) {
-
-  const [lineQuantity, setLineQuantity] = useState(line_item.quantity)
+const LineItem = ({ line_item, removeCartLines, updateCartLines }) => {
+  const [lineQuantity, setLineQuantity] = useState(line_item.quantity);
 
   const decrementQuantity = (lineId, merchandiseId) => {
-    const updatedQuantity = line_item.quantity - 1
-    updateCartLines(lineId, merchandiseId, updatedQuantity)
-    setLineQuantity(updatedQuantity)
-  }
+    const updatedQuantity = line_item.quantity - 1;
+    updateCartLines(lineId, merchandiseId, updatedQuantity);
+    setLineQuantity(updatedQuantity);
+  };
 
   const incrementQuantity = (lineId, merchandiseId) => {
-    const updatedQuantity = line_item.quantity + 1
-    updateCartLines(lineId, merchandiseId, updatedQuantity)
-    setLineQuantity(updatedQuantity)
-  }
+    const updatedQuantity = line_item.quantity + 1;
+    updateCartLines(lineId, merchandiseId, updatedQuantity);
+    setLineQuantity(updatedQuantity);
+  };
 
   return (
     <>
-      <li className={styles.cartLine}>
-        <div className={styles.imageContainer}>
+      <li key={line_item.id} className='flex py-6 sm:py-10'>
+        <div className='w-1/2 flex-shrink-0'>
           <img
-            className={styles.productImage}
-            alt={`${line_item.title}`}
+            className='w-full h-full rounded-md object-center object-cover'
             src={line_item.merchandise.image.originalSrc}
+            alt={`${line_item.title}`}
           />
         </div>
-        <div className={styles.lineDetails}>
-          <div className={styles.lineTitle}>
-            <span>
-              {line_item.merchandise.title}
-            </span>
-          </div>
-          <div className={styles.linePrice}>
-            <span>
-              ${(line_item.quantity * line_item.merchandise.price.amount).toFixed(2)}
-            </span>
-          </div>
-          <div className={styles.lineQuantity}>
-            <div className='input-group'>
-              <button variant='outline-secondary' type='button' onClick={() => decrementQuantity(line_item.id, line_item.merchandise.id)}>-</button>
-              <span className={styles.quantity}>{lineQuantity}</span>
-              <button variant='outline-secondary' type='button' onClick={() => incrementQuantity(line_item.id, line_item.merchandise.id)}>+</button>
+
+        <div className='ml-4 flex-1 flex flex-col justify-between sm:ml-6'>
+          <div className='relative pr-9 sm:gap-x-6 sm:pr-0'>
+            <div>
+              <div className='flex justify-between'>
+                <h3 className='text-sm'>
+                  <Link
+                    to={`${line_item.merchandise.product.productType}/${line_item.merchandise.product.handle}`}
+                    className='font-medium text-gray-700 hover:text-gray-800'>
+                    {line_item.merchandise.title}
+                  </Link>
+                </h3>
+              </div>
+
+              <p className='my-8 text-sm font-medium text-gray-900'>
+                ${line_item.merchandise.price.amount}
+              </p>
+            </div>
+
+            <div className='mt-4 sm:mt-0 sm:pr-9'>
+              <label htmlFor={`quantity-${line_item.id}`} className='sr-only'>
+                Quantity, {line_item.title}
+              </label>
+              <div className='mt-1 flex rounded-md shadow-sm'>
+                {lineQuantity > 1 ? (
+                  <button
+                    type='button'
+                    className='relative inline-flex items-center space-x-2 px-4 py-2 border border-indigo-500 text-sm font-medium rounded-l-md text-white focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 bg-gradient-to-b from-indigo-500 to-purple-500 hover:from-sky-600 hover:to-cyan-400'
+                    onClick={() =>
+                      decrementQuantity(line_item.id, line_item.merchandise.id)
+                    }>
+                    <MinusIcon
+                      className='h-5 w-5 text-white'
+                      aria-hidden='true'
+                    />
+                  </button>
+                ) : (
+                  <button
+                    type='button'
+                    className='relative inline-flex items-center space-x-2 px-4 py-2 border border-indigo-500 text-sm font-medium rounded-l-md text-white focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 bg-gradient-to-b from-indigo-500 to-purple-500 hover:from-sky-600 hover:to-cyan-400'
+                    disabled>
+                    <MinusIcon
+                      className='h-5 w-5 text-white'
+                      aria-hidden='true'
+                    />
+                  </button>
+                )}
+                <div className='-ml-px relative flex items-stretch flex-grow focus-within:z-10'>
+                  <span className='inline-flex items-center justify-center focus:ring-indigo-500 focus:border-indigo-500 block w-full rounded-none rounded-l-md sm:text-sm border-indigo-500'>
+                    {lineQuantity}
+                  </span>
+                </div>
+                <button
+                  type='button'
+                  className='-ml-px relative inline-flex items-center space-x-2 px-4 py-2 border border-indigo-500 text-sm font-medium rounded-r-md text-white focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 bg-gradient-to-b from-indigo-500 to-purple-500 hover:from-sky-600 hover:to-cyan-400'
+                  onClick={() =>
+                    incrementQuantity(line_item.id, line_item.merchandise.id)
+                  }>
+                  <PlusIcon className='h-5 w-5 text-white' aria-hidden='true' />
+                </button>
+              </div>
+
+              <div className='absolute top-0 right-0'>
+                <button
+                  type='button'
+                  className='-m-2 p-2 inline-flex text-gray-400 hover:text-gray-500'
+                  onClick={() => removeCartLines(line_item.id)}>
+                  <span className='sr-only'>Remove</span>
+                  <TrashIcon className='h-5 w-5' aria-hidden='true' />
+                </button>
+              </div>
             </div>
           </div>
-        </div>
-        <div>
-          <button bsPrefix='removeButton' className={styles.removeButton} onClick={() => removeCartLines(line_item.id)}><TrashIcon/> Remove</button>
+
+          {/* <p className="mt-4 flex text-sm text-gray-700 space-x-2">
+                      {product.inStock ? (
+                        <CheckIcon className="flex-shrink-0 h-5 w-5 text-green-500" aria-hidden="true" />
+                      ) : (
+                        <ClockIcon className="flex-shrink-0 h-5 w-5 text-gray-300" aria-hidden="true" />
+                      )}
+
+                      <span>{product.inStock ? 'In stock' : `Ships in ${product.leadTime}`}</span>
+                    </p> */}
         </div>
       </li>
     </>
-  )
-}
+  );
+};
 
-export default LineItem
+export default LineItem;
